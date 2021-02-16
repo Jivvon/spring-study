5c1ed7771f56ce174c5c2a17ec286de052b0ed78
Jeong Ji Won <poiu8944@gmail.com>
2021-02-16 16:07:25 +0900

    context:component-scan 이용해서 bean 등록
    - 스프링 2.5부터 가능
    - Annotation 빈 등록 및 설정 방법

--- src/main/java/jiwon/study/springapplicationcontext/BookService.java
+++ src/main/java/jiwon/study/springapplicationcontext/BookService.java
@@ -1,7 +1,12 @@
 package jiwon.study.springapplicationcontext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service // bean으로 등록
 public class BookService {
 
+	@Autowired // bean 주입
 	BookRepository bookRepository;
 
 	public void setBookRepository(BookRepository bookRepository) {
