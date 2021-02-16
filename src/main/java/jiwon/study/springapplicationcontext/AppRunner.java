a2f85208b35d3ac9a7585628ca2cc95025fd240c
Jeong Ji Won <poiu8944@gmail.com>
2021-02-17 02:11:25 +0900

    Properties: Environment가 프로퍼티 소스 설정 및 프로퍼티 값을 가져온다
    - 우선순위 존재
    - @PropertySource: Environment를 통해 프로퍼티 추가하는 방법
    - 스프링 부트에서 기본 프로퍼티 소스 지원 (application.properties)

--- src/main/java/jiwon/study/springapplicationcontext/AppRunner.java
+++ src/main/java/jiwon/study/springapplicationcontext/AppRunner.java
@@ -2,6 +2,7 @@ package jiwon.study.springapplicationcontext;
 
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
@@ -17,10 +18,15 @@ public class AppRunner implements ApplicationRunner {
 	@Autowired
 	BookRepository bookRepository;
 
+	@Value("${app.name}")
+	String appName;
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		Environment environment = ctx.getEnvironment();
-		System.out.println(Arrays.toString(environment.getActiveProfiles()));
-		System.out.println(Arrays.toString(environment.getDefaultProfiles()));
+		System.out.println("environment.getProperty(\"app.name\") : " + environment.getProperty("app.name"));
+		// Run/Debug Configurations의 VM Options 안에 -Dapp.nameInOpt=springInOpt 추가
+		System.out.println("environment.getProperty(\"app.name\") : " + environment.getProperty("app.nameInOpt"));
+		System.out.println("Value ${app.name} : " + appName);
 	}
 }
--- src/main/java/jiwon/study/springapplicationcontext/DemoApplication.java
+++ src/main/java/jiwon/study/springapplicationcontext/DemoApplication.java
@@ -2,8 +2,10 @@ package jiwon.study.springapplicationcontext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("/app.properties")
 public class DemoApplication {
 
 	public static void main(String[] args) {
