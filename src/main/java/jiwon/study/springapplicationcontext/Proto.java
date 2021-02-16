1570053e795370929fd0baeedf404d54c5272a29
Jeong Ji Won <poiu8944@gmail.com>
2021-02-17 01:20:00 +0900

    Singleton 객체에서 Prototype 객체를 사용하려면 Proxy 패턴을 이용하자.
    - 스프링의 @Scope 어노테이션의 proxyMode 속성 사용
    - 다른 방법도 있지만 이 방법이 가장 POJO스럽다
    
      - POJO란: https://happyer16.tistory.com/entry/POJOplain-old-java-object%EB%9E%80

--- src/main/java/jiwon/study/springapplicationcontext/Proto.java
+++ src/main/java/jiwon/study/springapplicationcontext/Proto.java
@@ -1,9 +1,12 @@
 package jiwon.study.springapplicationcontext;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
-@Component @Scope("prototype")
+// 직접 참조하면 새로운 객체를 만들지 못하니까 class 기반의 proxy를 만들어서 이걸 사용해라.
+// INTERFACE도 같은 방법으로 사용 가능.
+@Component @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Proto {
 
 }
