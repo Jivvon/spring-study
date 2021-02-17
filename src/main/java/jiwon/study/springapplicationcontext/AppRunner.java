2015ac9f8e62f4105ea7a4371ec938bb91378287
Jeong Ji Won <poiu8944@gmail.com>
2021-02-17 17:46:31 +0900

    Resource의 타입은 location 문자열과 ApplicationContext의 타입에 따라 결정된다.
    - ApplicationContext의 타입에 상관없이 리소스 타입을 강제하려면 java.net.URL 접두어(+ classpath:) 중 하나를 사용할 수 있다.
      - classpath:jiwon/study/config.xml -> ClassPathResource
      - file:///some/resource/path/config.xml -> FileSystemResource

--- src/main/java/jiwon/study/springapplicationcontext/AppRunner.java
+++ src/main/java/jiwon/study/springapplicationcontext/AppRunner.java
@@ -17,7 +17,14 @@ public class AppRunner implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
+		System.out.println(resourceLoader.getClass());
+
+		// getResource의 파라미터로 그냥 이름만 적으면 ServletContextResource로 리졸빙이 된다.
+		// classpath: 접두어를 붙이면 ClassPathResource로 리졸빙이 되고,
+		// target/classes가 root가 되어 리소스를 찾는다.
 		Resource resource = resourceLoader.getResource("classpath:test.txt");
+		System.out.println(resource.getClass());
+
 		System.out.println(resource.exists());
 		System.out.println(resource.getDescription());
 		System.out.println(Files.readString(Path.of(resource.getURI())));
